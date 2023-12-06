8efc26616bef7054903f442782b62df4d6f56d55
Peter <peter@talesofapleb.com>
2023-12-06 07:55:42 +0000

    Fix building as a git dependency

--- android/build_libsecp256k1.sh
+++ android/build_libsecp256k1.sh
@@ -5,6 +5,11 @@ echo "$0"
 echo "$1"
 echo "$2"
 
+pushd "$scriptDir/../" || exit
+
+# Dart and Flutter don't support submodules for git dependencies
+git submodule update --init --recursive
+
 pushd "$scriptDir/../contrib/tap-protocol" || exit
 echo "ANDROID_NDK=\"$1\" ARCHS=\"$2\" \"$(pwd)/tools/build_android.sh\"" > "$scriptDir/last_run_command_$2.log"
 ANDROID_NDK="$1" ARCHS="$2" "$(pwd)/tools/build_android.sh"
@@ -23,5 +28,7 @@ ANDROID_NDK="$1" ARCHS="$2" "$(pwd)/tools/build_android.sh"
 #ARCHS=x86 ANDROID_NDK="$1" tools/build_android.sh
 #echo 
 #ARCHS=arm6d4-v8a ANDROID_NDK="$1" tools/build_android.sh
+
+popd || exit # tap-protocol
 
 popd || exit
