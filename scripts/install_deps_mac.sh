ff028eb82e6f2c08c950a5d016934437a46787e2
Peter <peter@talesofapleb.com>
2023-12-10 11:55:08 +0000

    Add script to install dependencies

---
+++ scripts/install_deps_mac.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+pushd "$(dirname "$0")/.." || exit
+
+# Ensure homebrew is installed
+if [ ! -x /opt/homebrew/bin/brew ]
+then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# tap-protocol dependencies
+brew install autoconf automake jq libtool
+
+# Setup SDKs
+export DEPS="$(pwd)/.deps"
+mkdir -p "$DEPS"
+  pushd "$DEPS" || exit
+    curl -o android.zip https://dl.google.com/android/repository/commandlinetools-mac-10406996_latest.zip
+    unzip -n android.zip -d android
+
+    pushd android/cmdline-tools || exit
+      mkdir latest
+      mv -fv bin lib *.* latest/
+      echo y | latest/bin/sdkmanager "build-tools;33.0.2"
+      echo y | latest/bin/sdkmanager "cmake;3.18.1"
+      echo y | latest/bin/sdkmanager "ndk;21.4.7075529"
+      echo y | latest/bin/sdkmanager "platform-tools"
+      echo y | latest/bin/sdkmanager "platforms;android-33"
+      echo y | latest/bin/sdkmanager "sources;android-33"
+    popd || exit # android/cmdline-tools
+
+    export ANDROID_HOME="$(pwd)/android"
+    export ANDROID_SDK_ROOT="$ANDROID_HOME"
+    export ANDROID_NDK_HOME="$ANDROID_HOME/ndk"
+
+    curl -o flutter.zip https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_arm64_3.7.12-stable.zip
+    unzip -n flutter.zip
+    export PATH="$(pwd)/flutter/bin:$PATH"
+  popd || exit # $DEPS
+popd || exit # Project directory
